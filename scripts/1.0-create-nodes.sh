dcb9646a95994f2dfc1f72d7cffe19e136128202
Evgeny Rozanov <e.rosanov@gmail.com>
2022-10-12 01:03:06 +0400

    scripts

--- scripts/1.0-create-nodes.sh
+++ scripts/1.0-create-nodes.sh
@@ -3,9 +3,9 @@ mkdir devnet && cd devnet
 
 mkdir node1
 # mkdir node1 node2
- cd node1 && echo 'test password' > password.txt
+#  cd node1 && echo 'd321k127' > password.txt
 
-# create accounts for node
+# create accounts for node 
 # cd ../
-# geth --datadir devnet/node1 account new --password devnet/node1/password.txt | grep -Eo '0[xX][0-9a-fA-F]+'  >>  devnet/node1/account.txt
+#  geth --datadir node1 account new --password node1/password.txt | grep -Eo '0[xX][0-9a-fA-F]+'  >  node1/account.txt
 
--- scripts/1.5-start-nodes.sh
+++ scripts/1.5-start-nodes.sh
@@ -5,6 +5,8 @@ PORT=30306
 
  echo $BOOT_NODE
 
+ echo 'd321k127' > devnet/node1/password.txt
+
 ../build/bin/geth --datadir devnet/node1 --port $PORT --bootnodes $BOOT_NODE  --networkid 123454321 --unlock 0x221fB65CdB12Cc5eC0f9a2AfEe52D6c5CeF2B8bb --password devnet/node1/password.txt
 
 
