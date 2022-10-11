286471342426a7593faed68d4a5d8bb6284858ca
Evgeny Rozanov <e.rosanov@gmail.com>
2022-10-11 22:12:54 +0400

    default genesis

--- scripts/1.0-create-nodes.sh
+++ scripts/1.0-create-nodes.sh
@@ -2,11 +2,8 @@
 mkdir devnet && cd devnet
 mkdir node1 node2
 cd node1 && echo 'test password' > password.txt
-cd ../node2 && echo 'test password' > password.txt
 
-
-//create accounts for nodes
+//create accounts for node
 cd ../
 geth --datadir node1 account new --password node1/password.txt | grep -Eo '0[xX][0-9a-fA-F]+'  >>  node1/account.txt
-geth --datadir node2 account new --password node2/password.txt | grep -Eo '0[xX][0-9a-fA-F]+'  >>  node2/account.txt
 
