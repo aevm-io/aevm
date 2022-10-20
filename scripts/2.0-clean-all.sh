b6c8d70ccc53ad196a5939538d141913ed4d7143
Evgeny Rozanov <e.rosanov@gmail.com>
2022-10-20 16:58:35 +0400

    fix scripts

--- scripts/2.0-clean-all.sh
+++ scripts/2.0-clean-all.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sh ./1.4-kill-bootnode.sh
-sh ./1.5-kill-nodes.sh
+sh ./1.3-kill-bootnode.sh
+sh ./1.4-kill-nodes.sh
 sh ./1.0-clean-nodes.sh
--- scripts/2.0-start-all.sh
+++ scripts/2.0-start-all.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
  ./1.0-create-nodes.sh
- ./1.2-init-nodes.sh
- ./1.3-init-bootnode.sh
- ./1.4-start-bootnode.sh
+ ./1.1-init-nodes.sh
+ ./1.2-init-bootnode.sh
+ ./1.3-start-bootnode.sh
